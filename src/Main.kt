c0c162c0ce0534ed2636fd4460fc0d9ce8b1fa21
Vorkytaka <solidlsnake@yandex.ru>
2019-03-25 23:23:29 +0300

    • Init commit • Base of project

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+}
+
+fun main(args: Array<String>) {
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        watchPaths = listOf("MainKt"),
+        module = Application::module
+    ).start()
+}

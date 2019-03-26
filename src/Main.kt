724962e485528a19b684245716115f0a39fce9df
Vorkytaka <solidlsnake@yandex.ru>
2019-03-26 22:32:27 +0300

    • Create testing database

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,52 @@
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
+import io.ktor.jackson.jackson
+import io.ktor.routing.Routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import model.Posts
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import routing.api
+import service.ApiService
 
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
+
+    Database.connect(createTestDb())
+    transaction {
+        SchemaUtils.create(Posts)
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+        }
+    }
+
+    install(Routing) {
+        api(ApiService())
+    }
+}
+
+fun createTestDb(): HikariDataSource {
+    val config = HikariConfig().apply {
+        driverClassName = "org.h2.Driver"
+        jdbcUrl = "jdbc:h2:mem:test"
+        maximumPoolSize = 3
+        isAutoCommit = false
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        validate()
+    }
+    return HikariDataSource(config)
 }
 
 fun main(args: Array<String>) {

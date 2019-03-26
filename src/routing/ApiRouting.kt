006ccaa4ace6e0b7fa1fe83022d784abc362f5b5
Vorkytaka <solidlsnake@yandex.ru>
2019-03-26 22:54:44 +0300

    • Add new post in routing (by POST request)

--- src/routing/ApiRouting.kt
+++ src/routing/ApiRouting.kt
@@ -2,10 +2,13 @@ package routing
 
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
+import model.NewPost
 import service.IApiService
 
 fun Route.api(service: IApiService) {
@@ -23,6 +26,11 @@ fun Route.api(service: IApiService) {
             } else {
                 call.respond(HttpStatusCode.NotFound)
             }
+        }
+
+        post("/new") {
+            val post = call.receive<NewPost>()
+            call.respond(HttpStatusCode.Created, service.addPost(post))
         }
     }
 }

064887e93609a55fb268fe4110a47338f2073f04
Vorkytaka <solidlsnake@yandex.ru>
2019-04-19 17:38:35 +0300

    • Create tables for Tags and references

--- src/data/table/Posts.kt
+++ src/data/table/Posts.kt
@@ -4,6 +4,7 @@ import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.LongEntity
 import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.LongIdTable
+import org.jetbrains.exposed.sql.Table
 
 object Posts : LongIdTable() {
     val author = varchar("author", 64).nullable()
@@ -23,4 +24,22 @@ class PostEntity(
     var content by Posts.content
     var createdDate by Posts.createdDate
     var language by Posts.language
+    var tags by TagEntity via PostTags
+}
+
+object Tags : LongIdTable() {
+    val name = varchar("name", 64).uniqueIndex()
+}
+
+class TagEntity(
+    id: EntityID<Long>
+) : LongEntity(id) {
+    companion object : LongEntityClass<TagEntity>(Tags)
+
+    var name by Tags.name
+}
+
+object PostTags : Table() {
+    val post = reference("post", Posts).primaryKey(0)
+    val tag = reference("tag", Tags).primaryKey(1)
 }
